0e7119486c1c501a0972d2765c8cd252e2695094
Fizioh <batrom@laposte.net>
2021-04-05 11:46:02 +0200

    some styles + change function app to class componenté

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Search from './Components/Search'
 
 
-export default function App() {
-  return (
-    <Search />
-  );
+export default class App extends React.Componenent() {
+  render(){
+    return (
+      <Search />
+    );
+
+  }
 }
--- Components/Search.js
+++ Components/Search.js
@@ -4,9 +4,9 @@ import {View, Button, TextInput } from 'react-native'
 class Search extends React.Component {
     render(){
         return (
-            <View>
-                <TextInput placeholder="Titre du film" />
-                <Button title="Rechercher" onPress={() => {}} />
+            <View style={{marginTop: 20 }}>
+                 <TextInput style={{ marginLeft: 5, marginRight: 5, height: '50%', borderColor: '#000000', borderWidth: 1, paddingLeft: 5 }} placeholder='Titre du film'/>
+                <Button style={{ height: 50}} title="Rechercher" onPress={() => {}} />
             </View>
         )
     }
