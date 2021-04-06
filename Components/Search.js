5f113cfff773040c03a2e4c1621548fda9c88bb5
Fizioh <batrom@laposte.net>
2021-04-06 19:31:17 +0200

    remove button style

--- Components/Search.js
+++ Components/Search.js
@@ -6,7 +6,7 @@ class Search extends React.Component {
         return (
             <View style={styles.main_container}>
                  <TextInput style={styles.texinput} placeholder='Titre du film'/>
-                <Button style={{ height: 50}} title="Rechercher" onPress={() => {}} />
+                <Button title="Rechercher" onPress={() => {}} />
             </View>
         )
     }
