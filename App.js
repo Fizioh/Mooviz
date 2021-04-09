7db7c248aa69ec48f5c57fba4e624062476941ac
Fizioh <batrom@laposte.net>
2021-04-09 14:55:37 +0200

    fix typo in app

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Search from './Components/Search'
 
 
-export default class App extends React.Component() {
+export default class App extends React.Component {
   render(){
     return (
       <Search />
