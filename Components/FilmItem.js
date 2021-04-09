24cfbb678927ce5793dd41ebf1146735287cdb9e
Fizioh <batrom@laposte.net>
2021-04-09 22:23:30 +0200

    essai realisation item

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -5,7 +5,26 @@ class FilmItem extends React.Component {
     render() {
         return (
             <View style={styles.main_container}>
-                <Text style={styles.title_text}>Titre du film</Text>
+                <View>
+
+                <View style={styles.content}>
+                    <Text style={styles.title_text}>Titre du film</Text>   
+                </View>
+
+                <View>
+                    <Text style={styles.vote_text}>Vote</Text>
+                </View> 
+
+                </View>
+
+                <View style={styles.description}>
+                    <Text style={styles.description_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Text>
+                </View>
+
+                <View style={styles.date}>
+                    <Text>Sorti le 09/09/1996</Text>
+                </View>
+                
             </View>
         )
     }
@@ -13,10 +32,22 @@ class FilmItem extends React.Component {
 
 const styles = StyleSheet.create({
     main_container: {
-        height: 190
+        height: 190,
+        flexDirection: 'row'
     },
-    title_text: {
-
+    content: {
+        flexDirection: 'column',
+        backgroundColor: 'green'
+    },
+    header: {
+        flexDirection: 'row',
+        backgroundColor: 'orange'
+    },
+    description: {
+        backgroundColor: 'red'
+    },
+    date: {
+        backgroundColor: 'pink'
     }
 
 })
