0372f62da622318fb2de5cd7a185fed3fcf1b0dc
Fizioh <batrom@laposte.net>
2021-04-10 12:44:42 +0200

    solution film item

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,30 +1,33 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 
 class FilmItem extends React.Component {
     render() {
         return (
             <View style={styles.main_container}>
-                <View>
 
-                <View style={styles.content}>
-                    <Text style={styles.title_text}>Titre du film</Text>   
+                <Image
+                style={styles.image}
+                source={{url: "image"}}
+                />
+              
+
+                <View style={styles.content_container}>
+                    <View style={styles.header_container}>
+                        <Text style={styles.title_text}>Titre du film</Text>
+                        <Text style={styles.vote_text}>7.2</Text> 
+                    </View>
+                      
+
+                    <View style={styles.description_container}>
+                        <Text style={styles.description_text} numberOfLine={6}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Text>
+                    </View>
+
+                    <View style={styles.date_container}>
+                        <Text style={styles.date_text}>Sorti le 09/09/1996</Text>
+                    </View>
+
                 </View>
-
-                <View>
-                    <Text style={styles.vote_text}>Vote</Text>
-                </View> 
-
-                </View>
-
-                <View style={styles.description}>
-                    <Text style={styles.description_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Text>
-                </View>
-
-                <View style={styles.date}>
-                    <Text>Sorti le 09/09/1996</Text>
-                </View>
-                
             </View>
         )
     }
@@ -35,21 +38,46 @@ const styles = StyleSheet.create({
         height: 190,
         flexDirection: 'row'
     },
-    content: {
-        flexDirection: 'column',
-        backgroundColor: 'green'
+    image: {
+        width: 120,
+        height: 180,
+        margin: 5,
+        backgroundColor: 'gray'
     },
-    header: {
-        flexDirection: 'row',
-        backgroundColor: 'orange'
+    content_container: {
+        flex: 1,
+        margin: 5
     },
-    description: {
-        backgroundColor: 'red'
+    header_container: {
+        flex: 3,
+        flexDirection: 'row'
     },
-    date: {
-        backgroundColor: 'pink'
+    title_text: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        flex: 1,
+        flexWrap: 'wrap',
+        paddingRight: 5
+    },
+    vote_text: {
+        fontWeight: 'bold',
+        fontSize: 26,
+        color: '#666666'
+    },
+    description_container: {
+        flex: 7
+    },
+    description_text: {
+        fontStyle: 'italic',
+        color: '#666666'
+    },
+    date_container: {
+        flex: 1
+    },
+    date_text: {
+        textAlign: 'right',
+        fontSize: 14
     }
-
 })
 
 export default FilmItem
