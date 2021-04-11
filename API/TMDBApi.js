7b6fd85db7659bd4de9c97b1797e3e57f61c461e
Fizioh <batrom@laposte.net>
2021-04-11 10:47:45 +0200

    add api fetch function

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,2 +1,8 @@
 const API_TOKEN = process.env.REACT_APP_GOOGLE_API_KEY
 
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url) 
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
