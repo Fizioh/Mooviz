933eda38e4e1cf0265e3bbda5ee81f8ede5ec3b1
Fizioh <batrom@laposte.net>
2021-04-11 19:39:05 +0200

    add activityLoader component

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList} from 'react-native'
+import { ActivityIndicator, StyleSheet, View, Button, TextInput, FlatList} from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -9,20 +9,35 @@ class Search extends React.Component {
         super(props)
         this.searchedText = "" // Init de la donnée searchedText en dehors du state
         this.state = {
-            films: []
+            films: [],
+            isLoading: false
         }
     }
 
     _loadFilms() {
         if(this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+            this.setState( { isLoading: true })
             getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-                this.setState({ films: data.results })
+                this.setState({ 
+                    films: data.results,
+                    isLoading: false
+                })
             })
         }
     }
 
     _searchTextInputChanged(text){
         this.searchedText = text // Modif du texte recherché à chaque saisie de texte sans passer par le setState
+    }
+
+    _displayLoading() {
+        if(this.state.isLoading) {
+            return(
+                <View style={styles.loeading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
     }
 
     render(){
@@ -40,6 +55,7 @@ class Search extends React.Component {
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -57,6 +73,15 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 
 })
