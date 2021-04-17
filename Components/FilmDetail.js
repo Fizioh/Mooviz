aa80f0890704bf9e6c44068f006716d97fe75b23
Fizioh <batrom@laposte.net>
2021-04-17 20:27:14 +0200

    add activityIndicator to component

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, ActivityIndicator } from 'react-native'
 
-class FilmDetail extends React.Component {
+        class FilmDetail extends React.Component {
+
+            constructor(props){
+                super(props)
+                this.state = {
+                    film: undefined,
+                    isLoading: true
+                }
+            }
+
+        _displayLoading(){
+            if(this.state.isLoading){
+                return (
+                    <View style={styles.loading_container}>
+                        <ActivityIndicator size='large' />
+                    </View>
+                )
+            }
+        }
+
     render() {
         return(
             <View style={styles.main_container}>
-                <Text>Détail du film {this.props.navigation.state.params.idFilm}</Text>
+                    {this._displayLoading()}
             </View>
         )
     }
@@ -14,6 +33,15 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1,
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
