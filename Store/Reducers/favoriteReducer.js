1c31f5e566e0d05456402208f4585f89375c6817
Fizioh <batrom@laposte.net>
2021-04-19 20:26:57 +0200

    init action toggle favorite

--- Store/Reducers/favoriteReducer.js
+++ Store/Reducers/favoriteReducer.js
@@ -3,6 +3,8 @@ const initalState = { favoritesFilm: [] }
 function toggleFavorite(state = initialState, action){
     let nextState
     switch(action.type){
+        case 'TOGGLE_FAVORITE':
+            return nextState
         default:
             return state
     }
